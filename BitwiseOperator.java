502c5903342db8874f7b5bae19eaa0730903268b
Justice Thinker <justicethinker2@gmail.com>
2024-10-12 16:45:49 +0000

    git commit-m " added more descriptive comments, basic knowledge on binary to understand"

--- BitwiseOperator.java
+++ BitwiseOperator.java
@@ -2,28 +2,38 @@ package operators;
 
 public class BitwiseOperator {
     public static void main(String[] args) {
-        byte val1 = 12;     // 0000 1100
-        byte val2 = 10;   // 0000 1010
-        byte res;
+        // Declare two byte variables and assign values
+        byte val1 = 12;  // 0000 1100 in binary (decimal value 12)
+        byte val2 = 10;  // 0000 1010 in binary (decimal value 10)
+        byte res;        // Variable to store results of bitwise operations
 
-        //12 = 8 + 4 = 0000 1100 => (0000 1100 + 1) = -(12 + 1) + - 13
-        res = (byte) ~val1; //bitwise compliment val1
-        System.out.println("The result: " + res);
+        // Bitwise complement of val1 (~ flips all bits)
+        res = (byte) ~val1; // ~val1 = 1111 0011 (two's complement form for -13)
+        System.out.println("The result of ~val1: " + res); // Output: -13
 
-        res = (byte) (val1 & val2); //bitwise AND operation
-        System.out.println("The result:" + res);
+        // Bitwise AND operation (val1 & val2)
+        // 0000 1100 (val1) & 0000 1010 (val2) = 0000 1000 (binary for 8)
+        res = (byte) (val1 & val2);
+        System.out.println("The result of val1 & val2: " + res); // Output: 8
 
-        res =(byte) (val1 | val2); //bitwise OR operarion
-        System.out.println("The result:" + res);
+        // Bitwise OR operation (val1 | val2)
+        // 0000 1100 (val1) | 0000 1010 (val2) = 0000 1110 (binary for 14)
+        res = (byte) (val1 | val2);
+        System.out.println("The result of val1 | val2: " + res); // Output: 14
 
-        res = (byte) (val1 ^ val2); //bitwise XOR operation
-        System.out.println("The result" + res);
+        // Bitwise XOR operation (val1 ^ val2)
+        // 0000 1100 (val1) ^ 0000 1010 (val2) = 0000 0110 (binary for 6)
+        res = (byte) (val1 ^ val2);
+        System.out.println("The result of val1 ^ val2: " + res); // Output: 6
 
-        res = (byte) (val1 << 2); //shift 2 bits to the left
-        System.out.println("The result:" + res);
+        // Bitwise left shift operation (val1 << 2)
+        // Shifts bits of val1 (0000 1100) two positions to the left: 0011 0000 (binary for 48)
+        res = (byte) (val1 << 2);
+        System.out.println("The result of val1 << 2: " + res); // Output: 48
 
-        res = (byte) (val1 >> 2); //shift 2 bits to the right
-        System.out.println("The result" + res);
-            
+        // Bitwise right shift operation (val1 >> 2)
+        // Shifts bits of val1 (0000 1100) two positions to the right: 0000 0011 (binary for 3)
+        res = (byte) (val1 >> 2);
+        System.out.println("The result of val1 >> 2: " + res); // Output: 3
     }
 }
